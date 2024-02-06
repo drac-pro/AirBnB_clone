480bedf70a8c832cf9dcfbe232a28672bd9a6dab
Darius Tohtin <dariustohtin@gmail.com>
2024-02-06 10:50:03 -0800

    Created models/base_model.py and models/__init__.py

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import uuid
+from datetime import datetime
+"""Defines a class BaseModel"""
+
+
+class BaseModel:
+    """defines a BaseModel object"""
+    def __init__(self, *args, **kwargs):
+        """Initializes a BaseModel object
+        Args:
+            args: variable args
+            kwargs: variable key/value args
+        """
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ("created_at", "updated_at"):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current
+        datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the
+        instance"""
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
+
+    def __str__(self):
+        """string representation of the class instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
