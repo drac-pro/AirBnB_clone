dcf3600711c3089809666075cac04984fc5c105b
Darius Tohtin <dariustohtin@gmail.com>
2024-02-09 06:29:09 -0800

    Created more classes

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""Defines a class City"""
+
+
+class City(BaseModel):
+    """Defines a City object"""
+    state_id = ""
+    name = ""
